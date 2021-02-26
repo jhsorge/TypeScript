0900d434dba2fbad920b408d8c36497f9ec23995
Jennifer Sorge <jennifersorge@gmail.com>
2021-02-26 20:28:05 +0100

    My first commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+function add(n1:number, n2:number) {
+    console.log(typeof number1);
+    return n1+n2;
+}
+function subtract (n1:number, n2:number) {
+    return n2-n1;
+}
+
+const number1 = 20;
+const number2 = 2.8;
+
+const result = add(subtract (number2, number1), number2);
+console.log(result);
